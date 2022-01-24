47ee7c13345caa9d62574ce288b2a95eab0a8ca8
Chandrashekhar Paliwal <copaliwal@gmail.com>
2022-01-24 17:45:12 +0530

    Service bus topic message sender

--- Controllers/ServiceBusDemoController.cs
+++ Controllers/ServiceBusDemoController.cs
@@ -13,9 +13,6 @@ namespace MessageBrokerDemo.Controllers
     [ApiController]
     public class ServiceBusDemoController : ControllerBase
     {
-        private static string connection_string = "Endpoint=sb://servicebus240122.servicebus.windows.net/;SharedAccessKeyName=send;SharedAccessKey=3bfxLGDA/w0VXNkOTMt//mk756sUmiplTd/em3ie/kM=;EntityPath=app_queue";
-        private static string queue_name = "app_queue";
-
         public ServiceBusDemoController()
         {
 
@@ -24,8 +21,22 @@ namespace MessageBrokerDemo.Controllers
         [HttpPost("queue/message")]
         public async Task PostMessageInQuiue([FromBody] Order order)
         {
+            string connection_string = "Endpoint=sb://servicebus240122.servicebus.windows.net/;SharedAccessKeyName=send;SharedAccessKey=3bfxLGDA/w0VXNkOTMt//mk756sUmiplTd/em3ie/kM=;EntityPath=app_queue";
+            string queue_name = "app_queue";
             ServiceBusClient serviceBusClient = new ServiceBusClient(connection_string);
             ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(queue_name);
+            ServiceBusMessage serviceBusMessage = new ServiceBusMessage(order.ToString());
+
+            await serviceBusSender.SendMessageAsync(serviceBusMessage);
+        }
+
+        [HttpPost("topic/message")]
+        public async Task PostMessageInTopic([FromBody] Order order)
+        {
+            string connection_string = "Endpoint=sb://servicebus240122.servicebus.windows.net/;SharedAccessKeyName=send;SharedAccessKey=PFjynsOrUOrEI943WnRo5IqScHQ6cbnCJbvkM6qSYTA=;EntityPath=app_topic";
+            string topic_name = "app_topic";
+            ServiceBusClient serviceBusClient = new ServiceBusClient(connection_string);
+            ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(topic_name);
             ServiceBusMessage serviceBusMessage = new ServiceBusMessage(order.ToString());
 
             await serviceBusSender.SendMessageAsync(serviceBusMessage);
