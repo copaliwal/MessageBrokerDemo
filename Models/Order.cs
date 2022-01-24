0c0f05f9067112b014a7da4ff61c031748ed1bd0
Chandrashekhar Paliwal <copaliwal@gmail.com>
2022-01-24 15:41:43 +0530

    Add project files.

---
+++ Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace MessageBrokerDemo.Models
+{
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
